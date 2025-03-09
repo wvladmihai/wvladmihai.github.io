8f27a34be7670d5d3158fbc3d6fd69981b1d010c
vv <futsu@example.com>
2025-03-09 16:13:26 +0000

    integrate shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <Button>My Button</Button>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
